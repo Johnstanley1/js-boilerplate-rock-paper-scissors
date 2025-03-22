43b804caf1abb5fc3019158765e3c5bd7005018a
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-03-21 21:27:44 -0400

    created BasePlayer class

--- RPS.py
+++ RPS.py
@@ -1,10 +1,16 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+import random
+from collections import Counter
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+class BasePlayer():
+    def __init__(self):
+        self.opponent_history = [] # Keeps track of the opponent's moves
+        self.current_play = None
 
-    return guess
+    def update(self, prev_play):
+        if prev_play:
+            self.opponent_history.append(prev_play)
+
+    def predict(self):
+        raise NotImplementedError("Subclasses must implement predict()")
