a8b3bb9d4806d4bf28cd73899e93a337c9c989e4
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-03-23 21:39:16 -0400

    created  anti - Quincy bot

--- RPS.py
+++ RPS.py
@@ -37,7 +37,8 @@ class DetectBot(BasePlayer):
             Kris(),
             Abbey()
         ]
-
+        self.bot_name = self.bots
+        
     def detect_bot(self, prev_play):  
         if prev_play == "": # check if opponent is
             super().reset_state()
@@ -56,19 +57,47 @@ class DetectBot(BasePlayer):
             if len(self.opponent_history)>= 6:
                 if self.opponent_history[0:6] == ["", "R", "R", "P", "P", "S"]:
                     self.bot_name = self.bots[0]
-                elif self.opponent_history[:6] == ["", "R", "R", "R", "R", "S"]:
+                elif self.opponent_history[0:6] == ["", "R", "R", "R", "R", "S"]:
                     self.bot_name = self.bots[1]
+                elif self.opponent_history[0:6] == ["", "P", "R", "R", "R", "R"]:
+                    self.bot_name = self.bots[2]
+                elif self.opponent_history[0:6] == ['', 'P', 'P', 'R', 'R', 'R']:
+                    self.bot_name = self.bots[3]
 
+    
+    def predict(self, prev_play):
+        # choose strategy according to opponent
+        if self.bot_name == self.bots[0]:
+            player_move = self.bot_name.predict(prev_play)
+        elif self.bot_name == self.bots[1]:
+            player_move = self.bot_name.predict(prev_play)
+        elif self.bot_name == self.bots[2]:
+            player_move = self.bot_name.predict(prev_play)
+        elif self.bot_name == self.bots[3]:
+            player_move = self.bot_name.predict(prev_play)
+        else:
+            return random.choice(['R', 'P', 'S'])  # default strategy
 
-bot = DetectBot()
+        self.player_history.append(player_move)
+        
+        return player_move
+            
 
 class Quincy(BasePlayer):
     def __init__(self):
         super().__init__()
 
+    def predict(self):
+        choices = ["R", "P", "S", "S", "R"]
+        return choices[self.counter % 5]
+
+
 class Mrugesh(BasePlayer):
     def __init__(self):
         super().__init__()
+
+    def predict(self):
+        op
 
 class Kris(BasePlayer):
     def __init__(self):
@@ -77,3 +106,9 @@ class Kris(BasePlayer):
 class Abbey(BasePlayer):
     def __init__(self):
         super().__init__()
+
+
+bot = DetectBot()
+
+def player(prev_play):
+    return bot.predict(prev_play)
